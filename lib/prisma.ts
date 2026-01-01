d1e682bc960419bd6be6284af5c660781c272815
xiao-0104 <2415281296@qq.com>
2026-01-02 01:19:20 +0800

    fix: 适配 Prisma 7 配置规范

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,14 @@
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['query', 'error', 'warn'] })
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // 关键点：手动从环境变量读取连接串，解决 Prisma 7 不允许在 schema 中定义 env 的限制
+    datasourceUrl: process.env.DATABASE_URL, 
+    log: ['query', 'error', 'warn'],
+  })
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
